4cee9843ed93450d1cfe22319fc596e3dd8886c4
Herve Senot <herve.senot@groundtruth.com.au>
2013-11-15 12:13:24 +1100

    Adding import of AGL's CSVs and staging to consumption query

---
+++ load/agl.sh
@@ -0,0 +1,10 @@
+# This scripts extracts information from an AGL CSV file
+
+# Removing trailing commas as they mess around with the CSV import
+DIR=$(cd $(dirname "$0"); pwd)
+cat $1 | sed 's/"A",/"A"/' > $DIR/out/nmi.txt
+
+# Loading the content of this temporary file in the staging table
+psql -U ee_app -d ee -c "truncate table staging_agl1"
+psql -U ee_app -d ee -c "copy staging_agl1 (accountnumber,nmi,devicenumber,devicetype,registercode,ratetypedescription,startdate,enddate,profilereadvalue,registerreadvalue,qualityflag) from '$DIR/out/nmi.txt' HEADER CSV"
+
