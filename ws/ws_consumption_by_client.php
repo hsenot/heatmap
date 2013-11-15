9f6c354d348f2ba600261041d8d52725f5415191
Herve Senot <herve.senot@groundtruth.com.au>
2013-11-15 17:59:52 +1100

    Added PHP web service and WEB-INF for Quercus

---
+++ ws/ws_consumption_by_client.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Returns the entire dataset for a given client
+ */
+
+# Includes
+require_once("inc/error.inc.php");
+require_once("inc/database.inc.php");
+require_once("inc/security.inc.php");
+require_once("inc/json.pdo.inc.php");
+
+# Performs the query and returns XML or JSON
+try {
+	$p_client_id = $_REQUEST['client_id'];
+
+	$sql = <<<ENDSQL
+select day as d,prd as p,kwh as k
+from consumption
+where client_id=$p_client_id
+ENDSQL;
+
+	//echo $sql;
+	$pgconn = pgConnection();
+
+    /*** fetch into an PDOStatement object ***/
+    $recordSet = $pgconn->prepare($sql);
+    $recordSet->execute();
+
+	// Required to cater for IE
+	header("Content-Type: text/html");
+	echo rs2json($recordSet);
+}
+catch (Exception $e) {
+	trigger_error("Caught Exception: " . $e->getMessage(), E_USER_ERROR);
+}
+
+?>
