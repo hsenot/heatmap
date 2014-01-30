23a4b0dfb438cc78316e1754c498d66efca40c58
Herve Senot <herve.senot@groundtruth.com.au>
2014-01-30 18:20:00 +1100

    Minor change

--- load/origin.sh
+++ load/origin.sh
@@ -4,20 +4,23 @@
 # sudo apt-get install python-pdfminer
 
 # M 10 parameter required so that each line in the input PDF is maintained as a line in the text output 
-pdf2txt -o /tmp/nmi.txt -M 10 $1
+echo "SMD file:",$1
+echo "Staging area:",$2
+
+pdf2txt -o $2/nmi.txt -M 10 $1
 
 # Extracting only lines that start with a day of the week abbreviation
-cat /tmp/nmi.txt | grep '^Mon \|^Tue \|^Wed \|^Thu \|^Fri \|^Sat \|^Sun ' > /tmp/nmi2.txt
-START_DATE_STR=`cat /tmp/nmi.txt | grep 'Date Range .* to ' | cut -d " " -f3`
+cat $2/nmi.txt | grep '^Mon \|^Tue \|^Wed \|^Thu \|^Fri \|^Sat \|^Sun ' > $2/nmi2.txt
+START_DATE_STR=`cat $2/nmi.txt | grep 'Date Range .* to ' | cut -d " " -f3`
 
 # Loading the content of this temporary file in the staging table
 DIR=$(cd $(dirname "$0"); pwd)
 psql -U ee_app -d ee -c "truncate table staging_origin1"
-psql -U ee_app -d ee -c "copy staging_origin1 (content) from '/tmp/nmi2.txt'"
+psql -U ee_app -d ee -c "copy staging_origin1 (content) from '$2/nmi2.txt'"
 
 # Cleaning up after oneself
-rm /tmp/nmi.txt
-rm /tmp/nmi2.txt
+rm $2/nmi.txt
+rm $2/nmi2.txt
 
 # Returns the start date string
 echo $START_DATE_STR
