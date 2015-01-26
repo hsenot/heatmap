81ed535894cc8d35724526317024afd7b8c8106a
Herve Senot <herve.senot@groundtruth.com.au>
2015-01-27 09:36:49 +1100

    AGL filtering of exports - case of a datafile for a client with solar

--- load/agl.sh
+++ load/agl.sh
@@ -1,11 +1,12 @@
 # This scripts extracts information from an AGL CSV file
 
 # Removing trailing commas as they mess around with the CSV import
-cat $1 | sed 's/"A",/"A"/' > $2/nmi.txt
+# The heaer line is filtered out
+cat $1 | sed 's/..$//' | grep ',"A"$' | grep '#E1' > $2/nmi.txt
 
 # Loading the content of this temporary file in the staging table
 psql -U ee_app -d ee -c "truncate table staging_agl1"
-psql -U ee_app -d ee -c "copy staging_agl1 (accountnumber,nmi,devicenumber,devicetype,registercode,ratetypedescription,startdate,enddate,profilereadvalue,registerreadvalue,qualityflag) from '$2/nmi.txt' HEADER CSV"
+psql -U ee_app -d ee -c "copy staging_agl1 (accountnumber,nmi,devicenumber,devicetype,registercode,ratetypedescription,startdate,enddate,profilereadvalue,registerreadvalue,qualityflag) from '$2/nmi.txt' CSV"
 
 # Cleaning up
 rm $2/nmi.txt
